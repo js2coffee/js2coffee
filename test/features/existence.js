3bb82295150cb368981d2c4d46f57ff0ee45104e
Rod Knowlton <codelahoma@gmail.com>
2011-08-13 12:42:57 -0500

    retrieve reserved words from coffeescript; removed use of reserved word 'yes' from a test

--- test/features/existence.js
+++ test/features/existence.js
@@ -1,15 +1,15 @@
 function ifChecks() {
-    if (x) { yes }
-    if (!x) { yes }
+    if (x) { yep }
+    if (!x) { yep }
 }
 function ifNullChecks() {
-    if (x==null) { yes }
+    if (x==null) { yep }
     if (x===null) { nah }
 }
 function voidChecks() {
-    if (x==void 0) { yes }
+    if (x==void 0) { yep }
     if (x===void 0) { nah }
-    if (x==void 1) { yes }
+    if (x==void 1) { yep }
 }
 function undefinedChecks() {
     if (typeof x == 'undefined') { nah }
@@ -19,14 +19,14 @@ function edgeCase() {
 }
 
 function unlessChecks() {
-    if (x!=null) { yes }
+    if (x!=null) { yep }
     if (x!==null) { nah }
     if (typeof x != 'undefined') { wat }
 }
 
 function whileAndFor() {
-    while (x==null) { yes }
-    while (x===null) { yes }
+    while (x==null) { yep }
+    while (x===null) { yep }
 
-    for (a;x==null;2) { yes }
+    for (a;x==null;2) { yep }
 }
