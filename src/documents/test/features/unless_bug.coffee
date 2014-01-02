af6292ee5d21826dee3e188250c6d81c87a084f2
Anton Wilhelm <timaschew@gmail.com>
2014-01-03 00:18:34 +0100

    extends test for #226

--- src/documents/test/features/unless_bug.coffee
+++ src/documents/test/features/unless_bug.coffee
@@ -2,3 +2,12 @@ if true
   foo()
 else
   bar()
+A()  if a
+unless b
+  notB()
+else if c
+  C()
+else unless d
+  notD()
+else
+  foobar()
--- src/documents/test/features/unless_bug.js
+++ src/documents/test/features/unless_bug.js
@@ -3,3 +3,14 @@ if (true)
 else {
  bar();
 }
+if (a)
+  A();
+if (!b)
+  notB();
+else if (c)
+  C();
+else if (!d)
+  notD();
+else {
+  foobar();
+}
