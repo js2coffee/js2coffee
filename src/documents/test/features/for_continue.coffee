1fc37f6312b2a5fd94610b5b8acc069ebf0d4008
Andrej Mihajlov <and@codeispoetry.ru>
2014-11-08 17:40:53 +0100

    Update tests with inline code

--- src/documents/test/features/for_continue.coffee
+++ src/documents/test/features/for_continue.coffee
@@ -1,4 +1,21 @@
 i = 0
+while i < 2
+  i++
+  continue
+  i++
+i = 0
+while i < 3
+  if i is 2
+    i++
+    continue
+  i++
+i = 0
+while i < 4
+  alert i
+  i++
+  continue
+  i++
+i = 0
 while i < 5
   if i is 2
     ++i
@@ -6,7 +23,21 @@ while i < 5
   alert i
   ++i
 i = 0
-while i < 5
+while i < 6
+  if i is 2
+    i++
+    continue
+  j = 0
+  while j < 6
+    if i is j
+      alert j
+    else
+      j++
+      continue
+    j++
+  i++
+i = 0
+while i < 7
   switch i
     when 1
       alert "one"
--- src/documents/test/features/for_continue.js
+++ src/documents/test/features/for_continue.js
@@ -1,20 +1,40 @@
-for(i = 0; i <5; ++i) {
-   if(i == 2) {
-       continue;
-   }
-   alert(i);
+for(i = 0; i < 2; i++) continue;
+
+for(i = 0; i < 3; i++) if(i == 2) continue;
+
+for(i = 0; i < 4; i++) {
+  alert(i);
+  continue;
 }
 
-for(i = 0; i <5; ++i) {
-   switch(i) {
-   case 1:
-       alert("one");
-       break;
-   case 2:
-   case 3:
-       continue
-       break;
-   default:
-       alert(i);
-   }
+for(i = 0; i < 5; ++i) {
+  if(i == 2) {
+    continue;
+  }
+  alert(i);
+}
+
+for(i = 0; i < 6; i++) {
+  if(i == 2) continue;
+  for(j = 0; j < 6; j++) {
+    if(i == j) {
+      alert(j);
+    } else {
+      continue;
+    }
+  }
+}
+
+for(i = 0; i < 7; ++i) {
+  switch(i) {
+  case 1:
+    alert("one");
+  break;
+  case 2:
+  case 3:
+    continue;
+  break;
+  default:
+    alert(i);
+  }
 }
