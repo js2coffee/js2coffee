2db22621e7febba0989a4103e149c0fa6054b14e
Rico Sta. Cruz <hi@ricostacruz.com>
2015-01-24 00:51:27 +0800

    Update

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,23 @@
 ;(function () {
 
   ready(function () {
-    on(q('#src'), 'input', function () {
-      var input = this.value;
-      var output;
-
-      try {
-        output = Js2coffee.build(input);
-        q('#out').value = output.code;
-        q('#errors').style.display = 'none';
-      } catch (err) {
-        q('#errors').innerHTML = err.message;
-        q('#errors').style.display = 'block';
-      }
-    });
+    on(q('#src'), 'input', update);
+    update();
   });
+
+  function update() {
+    var input = q('#src').value;
+    var output;
+
+    try {
+      output = Js2coffee.build(input);
+      q('#out').value = output.code;
+      q('#errors').style.display = 'none';
+    } catch (err) {
+      q('#errors').innerHTML = err.message;
+      q('#errors').style.display = 'block';
+    }
+  }
 
   /*
    * Helpers taken from npmjs.com/dom101
