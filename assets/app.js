cc2599629ccea4f40bdfaa5bb4e327955dba21c6
Rico Sta. Cruz <hi@ricostacruz.com>
2015-02-01 00:57:34 +0800

    Update default text

--- assets/app.js
+++ assets/app.js
@@ -26,9 +26,9 @@ Editors.prototype = {
 
   defaultText: [
     '/*',
-    ' * Welcome to the new js2coffee, now',
-    ' * rewritten to use esprima, currently',
-    ' * in development.',
+    ' * Welcome to the new js2coffee 2.0, now',
+    ' * rewritten to use the esprima parser.',
+    ' * try it out!',
     ' */',
     '',
     'function listen (el, event, handler) {',
